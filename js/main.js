c9c0752d0f704533257a4ce044d7c305f941f565
Thiago <thiagocordeirodantas0@gmail.com>
2023-02-22 20:07:32 -0300

    criando as validacoes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
 const form = document.getElementById("novoItem");
+const lista = document.getElementById('lista');
 
 form.addEventListener('submit',((event) => {
     event.preventDefault()
-    console.log(event)
-    console.log(event.target.elements['nome'].value)
-    console.log(event.target.elements['quantidade'].value)
 
+
+    criaElemento(event.target.elements['nome'].value, event.target.elements['quantidade'].value)
 }))
+
+
+function criaElemento(nome, quantidade){
+    console.log(nome)
+    console.log(quantidade)
+
+
+    const novoItem = document.createElement('li');
+        novoItem.classList.add('item');
+    
+    const numeroItem = document.createElement('strong');
+        numeroItem.innerHTML = quantidade
+
+    novoItem.appendChild(numeroItem) 
+        novoItem.innerHTML += nome
+
+    
+    lista.appendChild(novoItem)
+    
+}
